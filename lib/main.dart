227a1ed3569599cb8181f59bfaa54144703f7a59
bsc-inf-11-19 <97790748+chifuniroChaguza@users.noreply.github.com>
2024-07-13 17:01:48 +0300

    Fixed main.dart to ensure that the provider is set up correctly.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:camera/camera.dart';
+import 'package:real_time_home_surveillance_system/providers/camera_controller_provider.dart';
 import 'package:real_time_home_surveillance_system/screens/home_screen.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final cameras = await availableCameras();
+  final firstCamera = cameras.first;
+
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => CameraControllerProvider(firstCamera),
+        ),
+      ],
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
