a3d171c0ea485c7547f71dc6e7128d5ef4358246
liana chagunda <bsc-com-26-19@unima.ac.mw>
2024-07-22 08:32:40 +0200

    adding dependencies for the recording components to work

--- routes/recording.js
+++ routes/recording.js
@@ -1,7 +1,7 @@
 // routes/recording.js
 const express = require('express');
 const router = express.Router();
-const recordingController = require('../controllers/RecordingControllers');
+const recordingController = require('../controllers/RecordingController');
 
 router.post('/start', recordingController.startRecording);
 router.post('/stop', recordingController.stopRecording);
