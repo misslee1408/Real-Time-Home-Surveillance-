adf9c9dc19a168f8544f77d87767a6a95f276fde
bsc-com-ne-07-18 <bsc-com-ne-07-18@unima.ac.mw>
2024-07-23 19:35:54 +0200

    implemented capturing of images  upon motion detection

--- pir_sensor.py
+++ pir_sensor.py
@@ -1,13 +1,21 @@
 import RPi.GPIO as GPIO
 import time
+from picamera import PiCamera
 
 # Set up GPIO
 GPIO.setmode(GPIO.BCM)
 PIR_PIN = 17
 GPIO.setup(PIR_PIN, GPIO.IN)
 
+camera = PiCamera()
+
 def motion_detected(channel):
     print("Motion detected!")
+    # Capture image when motion is detected
+    timestamp = time.strftime("%Y%m%d_%H%M%S")
+    filename = f"motion_{timestamp}.jpg"
+    camera.capture(filename)
+    print(f"Motion captured: {filename}")
 
 GPIO.add_event_detect(PIR_PIN, GPIO.RISING, callback=motion_detected)
 
@@ -19,6 +27,5 @@ try:
 except KeyboardInterrupt:
     print("Exiting")
     GPIO.cleanup()
-
-
+    camera.close()
 
