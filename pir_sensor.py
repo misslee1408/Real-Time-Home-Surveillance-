4c06e738c401b4c451fa85997c6cceafc6ad541a
bsc-com-ne-07-18 <bsc-com-ne-07-18@unima.ac.mw>
2024-07-23 19:54:35 +0200

    changed the time  for sleep

--- pir_sensor.py
+++ pir_sensor.py
@@ -1,26 +1,22 @@
 import RPi.GPIO as GPIO
 import time
 from picamera import PiCamera
-
-# Set up GPIO
+  
 GPIO.setmode(GPIO.BCM)
-PIR_PIN = 17
+PIR_PIN=17
 GPIO.setup(PIR_PIN, GPIO.IN)
 
 camera = PiCamera()
 
-# Set video resolution and framerate (adjust as needed)
 camera.resolution = (640, 480)
 camera.framerate = 30
 
-# Variable to track whether we are currently recording
 recording = False
 
 def motion_detected(channel):
-    global recording
-    print("Motion detected!")
+    global recording  
+    print("motion detected!!!!!!")
     if not recording:
-        # Start recording video when motion is detected
         timestamp = time.strftime("%Y%m%d_%H%M%S")
         filename = f"motion_{timestamp}.h264"
         camera.start_recording(filename)
@@ -30,14 +26,14 @@ def motion_detected(channel):
 GPIO.add_event_detect(PIR_PIN, GPIO.RISING, callback=motion_detected)
 
 try:
-    print("PIR Module Test (Ctrl+C to exit)")
+    print("PIR Module test (Ctrl+c to exit)")
     while True:
-        time.sleep(1)
+        time.sleep(10)
 
 except KeyboardInterrupt:
     print("Exiting")
     GPIO.cleanup()
     if recording:
         camera.stop_recording()
+    camera.close
     camera.close()
-
