d364443f3e5fea3e5d81fbd5d417e84ec2929c4a
bsc-com-ne-07-18 <bsc-com-ne-07-18@unima.ac.mw>
2024-08-02 00:51:47 +0200

    Update camera_stream.sh and remove templates/index.html

--- camera_stream.sh
+++ camera_stream.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Start the camera stream using RTSP
- raspivid -o - -t 0 -w 1280 -h 720 -fps 30 | ffmpeg -re -f h264 -i - -vcodec copy -an -f rtsp -analyzeduration 1000000 -probesize 1000000 rtsp://localhost:8554/mystream
+raspivid -o - -t 0 -w 1280 -h 720 -fps 30 | ffmpeg -re -f h264 -i - -vcodec copy -an -f rtsp rtsp://localhost:8554/mystream
 
+ 
 
